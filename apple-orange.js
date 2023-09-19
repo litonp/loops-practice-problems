f823d1a57550127dee97eac225a92522740e9e1d
litonp <litondas7121@gmail.com>
2023-09-19 18:57:09 +0600

    problem

--- apple-orange.js
+++ apple-orange.js
@@ -1,7 +1,8 @@
+// problem solve
 var moneyGiven = 1000;
 var applePrice = 400;
 var orangePrice = 300;
 var expense = applePrice + orangePrice;
-// console.log(expense);
+console.log(expense);
 var remain = moneyGiven - expense;
 console.log(remain);
