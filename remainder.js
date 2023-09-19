b5c3b461b4a6a76c816d742c3374ec46ba667e48
litonp <litondas7121@gmail.com>
2023-09-19 19:04:14 +0600

    problems

---
+++ remainder.js
@@ -0,0 +1,9 @@
+var name1 = "I am going to be";
+var name2 = " an awesome web developer";
+
+var totalName = name1 + ' ' + name2;
+console.log(totalName);
+
+var number = 119;
+var remainder = number % 5;
+console.log(remainder);
