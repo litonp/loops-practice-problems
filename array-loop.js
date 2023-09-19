e661ab23f105f6b68d1a4beba086fc6bee680f0e
litonp <litondas7121@gmail.com>
2023-09-19 12:52:46 +0600

    contineu

--- array-loop.js
+++ array-loop.js
@@ -1,13 +1,14 @@
-// for (var i = 0; i < 10; i++) {
-//     console.log(i)
-// }
+for (var i = 0; i < 10; i++) {
+    console.log(i)
+}
 
 // target: display every elements of an array
-// var numbers = [45, 87, 89, 56, 32, 51, 25, 12, 13];
-// for (var i = 0; i < numbers.length; i++) {
-//     var number = numbers[i];
-//     console.log(number);
-// }
+
+var numbers = [45, 87, 89, 56, 32, 51, 25, 12, 13];
+for (var i = 0; i < numbers.length; i++) {
+    var number = numbers[i];
+    console.log(number);
+}
 
 var items = ['bottle', 'mouse', 'sunglass', 'pen', 'notbook'];
 for (var i = 0; i < items.length; i++) {
