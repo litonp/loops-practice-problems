ea606aa4c15c9b5368ffcac60a967dee46c8f1e6
litonp <litondas7121@gmail.com>
2023-09-19 12:55:07 +0600

    amni

--- break.js
+++ break.js
@@ -1,27 +1,27 @@
-// for (var i = 1; i <= 20; i++) {
-//     console.log(i);
-//     if (i > 5) {
-//         break;
-//     }
-// }
+for (var i = 1; i <= 20; i++) {
+    console.log(i);
+    if (i > 5) {
+        break;
+    }
+}
 
-// var roastGiven = 0;
-// while (roastGiven < 10) {
-//     console.log('roast den');
-//     roastGiven++;
-//     if (roastGiven > 4) {
-//         break;
-//     }
-// }
+var roastGiven = 0;
+while (roastGiven < 10) {
+    console.log('roast den');
+    roastGiven++;
+    if (roastGiven > 4) {
+        break;
+    }
+}
 
-// var items = ['bottle', 'mouse', 'sunglass', 'pen', 'notbook'];
-// for (var i = 0; i < items.length; i++) {
-//     var item = items[i]
-//     if (item == 'pen') {
-//         break;
-//     }
-//     console.log(item)
-// }
+var items = ['bottle', 'mouse', 'sunglass', 'pen', 'notbook'];
+for (var i = 0; i < items.length; i++) {
+    var item = items[i]
+    if (item == 'pen') {
+        break;
+    }
+    console.log(item)
+}
 
 var numbers = [45, 87, 89, 56, 120, 32, 51, 25, 12, 13];
 for (var i = 0; i < numbers.length; i++) {
